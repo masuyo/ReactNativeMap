42f53f1534b73a08f1db83bf4f33fc3f887fc25d
sinka-palinkasg <gabriella.sinka-palinkas@p92.hu>
2019-03-22 17:53:09 +0100

    map works

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import UsersMap from './components/UsersMap';
+
+import FetchLocation from './components/FetchLocation';
+
+export default class App extends React.Component {
+  getUserLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      console.log(position);
+    }, err => console.log(err));
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FetchLocation onGetLocation={this.getUserLocationHandler} />
+        <UsersMap />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
