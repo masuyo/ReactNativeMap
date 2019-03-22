8e3f45eae04fe12a2bbe137ea80f613f6f0a35e5
sinka-palinkasg <gabriella.sinka-palinkas@p92.hu>
2019-03-22 17:58:32 +0100

    initial region set

--- components/UsersMap.js
+++ components/UsersMap.js
@@ -5,7 +5,13 @@ import MapView from 'react-native-maps';
 const usersMap = props => {
     return (
         <View style={styles.mapContainer}>
-            <MapView style={styles.map}/>
+            <MapView style={styles.map}
+                initialRegion={{
+                    latitude: 47.5856087,
+                    longitude: 19.0443254,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+            }}/>
         </View>
     );
 };
@@ -13,7 +19,8 @@ const usersMap = props => {
 const styles = StyleSheet.create({
     mapContainer: {
         width: '100%',
-        height: 200
+        height: 200,
+        marginTop: 20
     },
     map: {
         width: '100%',
